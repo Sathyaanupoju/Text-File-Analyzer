802e85d059402fbc6c187b82a80000367e9eb675
Sathyaanupoju <eswarisatyasridurga450@gmail.com>
2026-01-21 15:36:57 +0530

    Create text_utils.py

---
+++ text_utils.py
@@ -0,0 +1,14 @@
+def word_count(text):
+    return len(text.split())
+
+def line_count(text):
+    return len(text.split('\n'))
+
+def char_count(text):
+    return len(text)
+
+def to_upper(text):
+    return text.upper()
+
+def to_lower(text):
+    return text.lower()
